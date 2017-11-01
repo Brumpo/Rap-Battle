9251f837888e11f993a11034fbe50a965f565f48
Brent Schroder <schroder.brent@gmail.com>
2017-10-31 21:35:18 -0600

    used kayne rest to add lyric functionality but broke css

--- src/app.js
+++ src/app.js
@@ -1,3 +1,104 @@
-$document.ready(function{
-  
+$(document).ready(function(){
+  var $trackl = $('#track_left');
+  var $trackr = $('#track_right');
+  var $tracklbutton = $('#tracklsub');
+  var $trackrbutton = $('#trackrsub');
+  var $cardl = $('#cardl');
+  var $cardr = $('#cardr');
+  var $spritel = $('#spriteleft');
+  var $spriter = $('#spriteright');
+  var $namel = $('#nameleft');
+  var $namer = $('#nameright');
+  var $lyricl = $('#lyricl');
+  var $lyricr = $('#lyricr');
+  var $fight = $('#fight');
+  var $form = $('form');
+  var $winnerl = $('#winnerl');
+  var $winnerr = $('#winnerr');
+  var lyricsl = '';
+  var lyricsr = '';
+  var kanyeAlbums = {
+    the_college_dropout: 'http://i.imgur.com/X2ruUdL.jpg',
+    late_registration:'http://lamusicblog.com/wp-content/uploads/2015/08/kanye-west-Late-Registration-cover.jpg',
+    graduation:'http://cdn3.pitchfork.com/albums/10462/afa9da9d.jpg',
+    heartbreak:'http://www.hypebeast.com/image/2008/11/kanye-west-808s-heartbreak-kaws-2.jpg',
+    mbdtf : 'http://www.missinfo.tv/wp-content/uploads/2010/11/kanye-west-my-beautiful-dark-twisted-fantasy-album-cover-4.png',
+    yeezus : 'https://qph.ec.quoracdn.net/main-qimg-0b4d3539b314fb898a95d424fe1af853',
+    the_life_of_pablo : 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/02/17/11/kanye-26de4d2f26f82661.jpg',
+    funzies :'https://media.giphy.com/media/51h03rLP33G5G/giphy.gif'
+  };
+  var getsprite = function(album){
+    if(album==='the_college_dropout'){
+      return kanyeAlbums.the_college_dropout;
+    }else if(album==='late_registration'){
+      return kanyeAlbums.late_registration;
+    }else if(album==='graduation'){
+      return kanyeAlbums.graduation;
+    }else if(album==='808s_&amp;_heartbreak'){
+      return kanyeAlbums.heartbreak;
+    }else if(album==='my_beautiful_dark_twisted_fantasy'){
+      return kanyeAlbums.mbdtf;
+    }else if(album==='yeezus'){
+      return kanyeAlbums.yeezus;
+    }else if(album==='the_life_of_pablo'){
+      return kanyeAlbums.the_life_of_pablo;
+    }else{
+      return kanyeAlbums.funzies;
+    }
+  }
+  var getcardl = function(){
+    let query= $trackl.val()
+    let album = ''
+    let sprite = ''
+    let xhr=$.getJSON('https://spoterfyproxy.herokuapp.com/api/track/'+query)
+    xhr.done(function(data){
+      lyricsl = data.lyrics;
+      album = data.album;
+      sprite = getsprite(album);
+      $spritel[0].src = sprite;
+      $namel[0].innerHTML=query;
+      $cardl[0].style = 'display:block;';
+    })
+  }
+  var getcardr = function(){
+    let query= $trackr.val()
+    let album = ''
+    let sprite = ''
+    let xhr=$.getJSON('https://spoterfyproxy.herokuapp.com/api/track/'+query)
+    xhr.done(function(data){
+      lyricsr = data.lyrics;
+      album = data.album;
+      sprite = getsprite(album);
+      $spriter[0].src = sprite;
+      $namer[0].innerHTML=query;
+      $cardr[0].style = 'display:block;';
+      $fight[0].style = 'display:block;';
+    })
+  }
+  var appendlyricsl = function(){
+    $lyricl[0].innerHTML = lyricsl
+    $lyricl[0].style = 'display:block;'
+  }
+  var appendlyricsr = function(){
+    $lyricr[0].innerHTML = lyricsr
+    $lyricr[0].style = 'display:block;'
+  }
+  var storewinner = function(e){
+    alert('we made it');
+    location.reload();
+  }
+  $tracklbutton.on('click',getcardl)
+  $trackrbutton.on('click',getcardr)
+  $fight.on('click', function(){
+    $form[0].style = 'display:none;';
+    $cardr[0].style = 'display:none;';
+    $cardl[0].style = 'display:none;';
+    $fight[0].style = 'display:none;';
+    appendlyricsl();
+    appendlyricsr();
+    $winnerr[0].style = 'display:block;';
+    $winnerl[0].style = 'display:block;';
+  })
+  $winnerr.on('click',storewinner);
+  $winnerl.on('click',storewinner);
 });
--- src/portal.js
+++ src/portal.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
-  $init=('#init');
-  $init.on('click',function(){
-    xhr= $.getJSON('')
+  let $init=$('#loginbutton');
+  var id= '770aa5b1d69e46af8f9afff39037cfc0'
+  var secret= '92ceab4daea24bf78badceddd8220eb1'
+    $init.click(function(){
+    // xhr= $.post('https://spoterfyproxy.herokuapp.com/api/token_grantype=client_credentials'
+    // var xhr= $.post('https://spoterfyproxy.herokuapp.com/api/token')
+    // var xhr = $.ajax({
+      //  url :'https://spoterfyproxy.herokuapp.com/authorize/?client_id=770aa5b1d69e46af8f9afff39037cfc0&response_type=code&redirect_uri=http%3A%2F%2Flocalhost:3000%2Fbattle%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09',
+      //  method: 'GET'
+     //(':92ceab4daea24bf78badceddd8220eb1')}
+      // body: {grant_type:'client_credentials'}
+    // })
+        //url: 'https://spoterfyproxy.herokuapp.com/api/token',
+        // method: 'POST',
+        // form: {
+        //   redirect_uri: 'http://localhost:3000',
+        //   grant_type: 'authorization_code'
+        // },
+        // headers: {
+        //   'Authorization': 'Basic ' + (TypedArray(id + ':' + secret).toString('base64'))
+        // },
+        // json: true
+      //});
+      //  var xhr = $.ajax({
+        //  method: "POST",
+        //  url: "https://spoterfyproxy.herokuapp.com/api/token",
+        //  headers: {Authorization:' Basic ' + btoa('x770aa5b1d69e46af8f9afff39037cfc0:92ceab4daea24bf78badceddd8220eb1 ')},
+        //  data: { grant_type: 'client_credentials' }
+      //  })
+      // })
+  // });
+    //   xhr.done(function(data){
+    //     console.log('gucci',data)
+    // })
+    // $.getJSON('https://spoterfyproxy.herokuapp.com/').done(function(data){
+      // console.log("Data",data);
+    // })
+
   })
-});
+// });
+// curl -X "POST" -H "Authorization: Basic 770aa5b1d69e46af8f9afff39037cfc0:92ceab4daea24bf78badceddd8220eb1" -d grant_type=client_credentials https://accounts.spotify.com/api/token
